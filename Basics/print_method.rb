33578662a94e16bcf67f1bdab05f6b2ec978f50f
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-02 22:17:27 -0400

    First Commit

---
+++ Basics/print_method.rb
@@ -0,0 +1,6 @@
+
+# print doesn't add a line breaker
+
+print "Hello" 
+print "World"  
+# = HelloWorld
---
+++ Basics/puts_method.rb
@@ -0,0 +1,17 @@
+
+# puts automactily adds a line breaker even if it doesn't output anything
+# semicolon acts as a terminal
+# semicolon are not common in Ruby
+# string can include letters, space, numbers, symbols 
+
+puts "Hello word"
+puts "My name is Joseph Arias"
+
+puts 17
+puts 3.1456783
+
+# Cannot add string and number
+puts 12 + 10
+puts "4" + "5" #45 concat
+
+
