cfe5212d235843a52ca68bc0688c2d5f9f0a6fc9
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-02 22:41:59 -0400

    The P Methods

---
+++ Basics/p_method.rb
@@ -0,0 +1,14 @@
+# The p method provides more details about the output
+
+puts "Diana"; # Diana
+p "Diana"; # "Diana"
+
+puts "Hi there, this is a
+big line break" 
+#  Hi there, this is a
+# big line break
+
+p "Hi there, this is a 
+big line break"
+# "Hi there, this is a \nbig line break"
+
