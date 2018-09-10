3b9e19d143837677f9f028820455f2d305c7ae95
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-09 21:59:59 -0400

    step methods

---
+++ Numbers_Booleans/step_method.rb
@@ -0,0 +1,4 @@
+0.step(85, 7) do |num|
+  puts "Alright, let's go up by 7"
+  puts "I'm now on #{num}"
+end
---
+++ Numbers_Booleans/upto_downto_methods.rb
@@ -0,0 +1,12 @@
+
+5.downto(0) do |current_number|
+  puts "We are currently on #{current_number}"
+end
+
+puts "LIFTOFF!!"
+
+5.upto(10) do |num|
+  puts "We are moving up, currently on #{num}"
+end
+
+puts "We here!"
